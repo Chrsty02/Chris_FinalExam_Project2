fcf9247343a18cd3b31a1b7c51b5b137c9c62b85
Chrsty02 <chrsty02@outlook.com>
2025-06-12 18:22:22 -0400

    first commit

---
+++ src/main/java/Chris_FinalExam_Project2/Chris_FinalExam_Project2/App.java
@@ -0,0 +1,13 @@
+package Chris_FinalExam_Project2.Chris_FinalExam_Project2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
